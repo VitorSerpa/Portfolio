fd53e049bb6629a7d4155bdb1b201579dfa3dbf6
VitorSerpa <vitorserpa123@gmail.com>
2025-04-06 16:45:39 -0300

    fix(Resposividade)

--- portfolio/src/components/Pages/NotFound.tsx
+++ portfolio/src/components/Pages/NotFound.tsx
@@ -3,6 +3,7 @@ const NotFound = () => {
       <main style={{ textAlign: "center", padding: "4rem" }}>
         <h1>404</h1>
         <p>Página não encontrada 😢</p>
+        
       </main>
     );
   };
