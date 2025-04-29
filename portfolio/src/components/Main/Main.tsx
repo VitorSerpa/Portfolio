d6d157592cbbaa383883c22978af111ab0dff392
VitorSerpa <vitorserpa123@gmail.com>
2025-04-28 22:39:44 -0300

    feat(new main and header desing)

--- portfolio/src/components/Main/Main.tsx
+++ portfolio/src/components/Main/Main.tsx
@@ -1,20 +1,35 @@
 import React, { FC } from "react";
 import styles from "./Main.module.css"
+import Button from "../Inputs/Button";
 
-const Main: FC = () =>{
+const Main: FC = () => {
     return (
         <main className={styles.main}>
-            <section className={styles.sectionTitle}>
-                <h1>Olá, eu sou o Vitor</h1>
-                <p>Desenvolvedor Full-Stack</p>
-                <div>
-                    <button>Baixe meu CV</button>
-                    <button>Entre em contato</button>
+            <section className={styles.banner}>
+                <div className={styles.intro}>
+                    <div className={styles.title}>
+                        <h1>Olá, eu sou o <a href=""><span className={styles.purpleTitle}>Vitor :)</span></a></h1>
+                        <p>Desenvolvedor de Software</p>
+                        <div className={styles.actions}>
+                            <a className={styles.button1} href="">Baixe meu CV!</a>
+                            <a className={styles.button2} href="">Contate-me!</a>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.profile}>
+                    <img className={styles.img} src="/images/VitorSerpa2bc.png" alt="" />
                 </div>
             </section>
-            <aside className={styles.sectionImg}>
-                <img src="" alt="Minha imagem" />
-            </aside>
+            <section className={styles.about}>
+                <h1>Sobre mim:</h1>
+                <p>Olá, meu nome é <b>Vitor Serpa</b>, apaixonado por tecnologia e programação, atualmente estudo na <b>FATEC-SJC</b>, cursando <b>Desenvolvimento de Software Multiplataforma</b>. Busco em minha jornada me especializar no desenvolvimento WEB e possuo e grande interesse na área de inteligência artificial e estrutura de dados.</p>
+            </section>
+            <div className={styles.scrollDown}>
+                <span className={styles.purpleTitle}>Role para baixo!</span>
+                <div className={styles.arrowWrapper}>
+                    <div className={styles.arrow}></div>
+                </div>
+            </div>
         </main>
     )
 }
