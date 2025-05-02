046b18721cd736ed6282f1dc50407426d8fbff2f
VitorSerpa <vitorserpa123@gmail.com>
2025-05-02 02:06:07 -0300

    feat(Projects Section finished):Project Section now have transistions and new tabs to better navigation

--- portfolio/src/components/Main/Main.tsx
+++ portfolio/src/components/Main/Main.tsx
@@ -27,18 +27,28 @@ const Main: FC = () => {
             projectName: "Projeto de reconhecimento de veículos e refatoração de semáforos",
             normalImg: "/images/normalImages/avenueImage.jpeg",
             darkImg: "/images/darkImages/avenueImage.png",
-            description: "Projeto de plataforma WEB e IA para reconhecimento de tráfego urbano e refatoração semáforica em conjunto com professor do ITA (Em desenvolvimento)."
-
+            description: "Projeto de plataforma WEB e IA parasenvolvimento).",
+            svgs: [
+                "python", "yolo", "opencv", "googlecolab", "react"
+            ]
         },
         {
-            projectName: "Refatroação do sistema de registro de presença escolar",
-            img: "/images/tccImage.jpeg",
-            description: "Ferramenta de visualização de tráfego urbano em tempo real com alertas e interface visual baseada em dados."
+            projectName: "Refatoração do sistema de registro de presença do Colégio UNIVAP",
+            normalImg: "/images/normalImages/tccImage.jpeg",
+            darkImg: "/images/darkImages/tccImage.png",
+            description: "Projeto de plataforma WEB e IA (Em desenvolvimento).",
+            svgs: [
+                1, 2
+            ]
         },
         {
-            projectName: "Refatroação do sistema de registro de presença escolar",
-            img: "/images/tccImage.jpeg",
-            description: "Ferramenta de visualização de tráfego urbano em tempo real com alertas e interface visual baseada em dados."
+            projectName: "Analise de cargas de SP",
+            normalImg: "/images/normalImages/analiseSP.png",
+            darkImg: "/images/darkImages/analiseSP.png",
+            description: "Projeto de plataforma WEB e IA  desenvolvimento).",
+            svgs: [
+                1, 2
+            ]
         },
     ]
 
@@ -80,15 +90,26 @@ const Main: FC = () => {
             </section>
             <section className={styles.projects}>
                 <h1>Projetos:</h1>
+                <h2>Destaques:</h2>
                 <div className={styles.projectsSection}>
                     {projectList.map((dic) => (
                         <div className={styles.projectCard} key={dic.projectName}>
-                            <img className={styles.normalImage} src={dic.normalImg} alt={dic.darkImg} />
+                            <img className={styles.darkImage} src={dic.darkImg} alt={dic.darkImg} />
+                            <img className={styles.normalImage} src={dic.normalImg} alt={dic.normalImg} />
                             <div className={styles.skillsProjectCard}>
                                 <h3>{dic.projectName}</h3>
-                            </div>  
+                                {dic.svgs.map((element, index) => (
+                                    <img src={`svg/${element}.svg`} alt="" className={styles.svg} />
+                                ))}
+                            </div>
+                            <div className={styles.hoverDescription}>
+                                <button className={styles.seeMore}>Veja mais!</button>
+                            </div>
                         </div>
                     ))}
+                </div>
+                <div className={styles.linkDiv}>
+                    <a href="/projetos">Veja meus projetos!</a>
                 </div>
             </section>
         </main>
