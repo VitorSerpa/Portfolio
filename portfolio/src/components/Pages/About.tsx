794faabde5b491291b4e2e973b348c39e6775125
VitorSerpa <vitorserpa123@gmail.com>
2025-05-12 10:31:23 -0300

    feat(About section updated)

--- portfolio/src/components/Pages/About.tsx
+++ portfolio/src/components/Pages/About.tsx
@@ -1,53 +1,79 @@
 import React, { FC } from "react";
 import styles from "./About.module.css";
+import { useNavigate } from "react-router-dom";
+
+
+
+const skillsList = [
+    ["HTML", "html5"],
+    ["CSS", "css"],
+    ["SASS", "sass"],
+    ["Javascript", "javascript"],
+    ["Typescript", "typescript"],
+    ["React", "react"],
+    ["Node.JS", "node"],
+    ["MySQL", "mysql"],
+    ["Java", "java"],
+    ["C#", "csharp"],
+    ["Unity", "unity"],
+    ["Python", "python"],
+    ["Pandas", "pandas"],
+    ["CTkinter", "custom tkinter"],
+    ["Flask", "flask"],
+    ["COLAB", "google colab"],
+    ["OpenCV", "opencv"],
+    ["YOLO", "yolo"],
+];
 
 const About: FC = () => {
+    const navigate = useNavigate();
+
+    const handleClickIcon = (icon: string) => {
+        navigate("/techs", { state: icon });
+    };
+
     return (
         <div className={styles.about}>
-            <h1>Sobre Mim:</h1>
-            <section className={styles.description}>
-                <div className={styles.descriptionSection}>
-                    <h2>Informações:</h2>
-                    <div>
-                        <h3>Brasileiro</h3>
-                        <p>R. Capitão Alípio Neves Barbosa, 84 - Jardim Portugal, São José dos Campos - SP </p>
-                        <p>
-                            <b>
-                                Olá, meu nome é Vitor Serpa, apaixonado por tecnologia e programação, atualmente
-                                estudo na FATEC-SJC, cursando Desenvolvimento de Software Multiplataforma.
-                                Busco me especializar no desenvolvimento WEB, com grande interesse na área de
-                                inteligência artificial e estrutura de dados.
-                            </b>
-                        </p>{" "}
-                        <br />
-                        <p>
-                            <b>
-                                Sou uma pessoa extrovertida, sociável e com excelente capacidade de adaptação. Tenho
-                                experiência com trabalho em equipe, sendo colaborativo e comunicativo.
-                                Possuo resiliência diante de desafios, sou proativo na busca por soluções e tenho
-                                facilidade para aprender com rapidez. Valorizo a escuta ativa, organização e estou
-                                sempre em busca de crescimento pessoal e profissional.
-                            </b>
-                        </p>
-                    </div>
-                    <h2>Formação acadêmica:</h2>
-                    <h3>Desenvolvimento de Software Multiplataforma – 1º Semestre | FATEC |</h3>
+            <h1>Sobre mim:</h1>
+            <section className={styles.welcomeSection}>
+                <div className={styles.welcomeTextDiv}>
+                    <h3>Brasileiro</h3>
+                    <p>R. Capitão Alípio Neves Barbosa, 84 - Jardim Portugal, São José dos Campos - SP </p> <br />
+                    <p>
+                        <b>
+                            Olá, meu nome é Vitor Serpa, apaixonado por tecnologia e programação, atualmente
+                            estudo na FATEC-SJC, cursando Desenvolvimento de Software Multiplataforma.
+                            Busco me especializar no desenvolvimento WEB, com grande interesse na área de
+                            inteligência artificial e estrutura de dados.
+                        </b>
+                    </p>{" "}
+                    <br />
+                    <p>
+                        <b>
+                            Sou uma pessoa extrovertida, sociável e com excelente capacidade de adaptação. Tenho
+                            experiência com trabalho em equipe, sendo colaborativo e comunicativo.
+                            Possuo resiliência diante de desafios, sou proativo na busca por soluções e tenho
+                            facilidade para aprender com rapidez. Valorizo a escuta ativa, organização e estou
+                            sempre em busca de crescimento pessoal e profissional.
+                        </b>
+                    </p>
                 </div>
-                <div className={styles.imageSection}>
-                    <img
-                        className={styles.img}
-                        src="/images/VitorSerpa2bc.png"
-                        alt="Imagem do Autor do site Vitor Serpa"
-                    />
-                    <div className={styles.actions}>
-                            <a className={styles.button1} href="">
-                                Baixe meu CV!
-                            </a>
-                            <a className={styles.button2} href="#contato">
-                                Contate-me!
-                            </a>
+                <div className={styles.actions}>
+                    <h2>Tecnologias</h2>
+                    <div className={styles.icons}>
+                    {skillsList.map(([label, icon]) => (
+                        <div className={styles.icon} key={icon}>
+                            <button className={styles.iconButton} onClick={() => handleClickIcon(icon)}>
+                                <p>{label}</p>
+                                <img className={styles.iconConfig} src={`/svg/${icon}.svg`} alt={label} />
+                            </button>
                         </div>
+                    ))}
                 </div>
+                </div>
+            </section>
+            <section className={styles.projectsSection}>
+                <h1>Projetos</h1>
             </section>
         </div>
     );
