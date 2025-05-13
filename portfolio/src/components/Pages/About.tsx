65c80789716bb16b3866a3ba962e2d74df2a65a0
VitorSerpa <vitorserpa123@gmail.com>
2025-05-13 10:23:19 -0300

    feat(New About design)

--- portfolio/src/components/Pages/About.tsx
+++ portfolio/src/components/Pages/About.tsx
@@ -72,13 +72,13 @@ const About: FC = () => {
                         <b>Email:</b> <a href="mailto:vitorserpa123@gmail.com" target="_blank">vitorserpa123@gmail.com</a>
                     </p>
                     <p>
-                        <b>Telefone:</b> <a href="https://wa.me/12981312111"target="_blank">GitHub</a>
+                        <b>Telefone:</b> <a href="https://wa.me/12981312111" target="_blank">GitHub</a>
                     </p>
                     <p>
                         <b>Linkedin:</b> <a href="https://www.linkedin.com/in/vitor-serpa-925b46322/" target="_blank">Linkedin</a>
                     </p>
                 </aside>
-                <h1><b>Formação acadêmica</b></h1>
+                <h1 className={styles.h1Formation}><b>Formação acadêmica:</b></h1>
                 <section className={styles.formation1}>
                     <h2><b>Desenvolvimento de Software Multiplataforma – 2025 | FATEC | </b></h2>
                     <ul>
@@ -98,6 +98,51 @@ const About: FC = () => {
                         <li>Python (pandas, matplotlib, etc).</li>
                         <li>Tecnologias como Unity2D, RAD (.NET Framework), Android Studio (Kotlin). </li>
                         <li>Programação orientada a objeto. </li>
+                    </ul>
+                </section>
+                <h1 className={styles.h1Formation}><b>Experiências:</b></h1>
+                <section className={styles.formation1}>
+                    <h2><b>FATEC-SJC</b></h2>
+                    <h3>Atuação em desenvolvimento de projeto como P.O (Product Owner) | 2025 |</h3>
+                    <ul>
+                        <li>Criação de plataforma WEB para análise gráfica de importações e exportações
+                            realizadas por todas as cidades do Estado de São Paulo.</li>
+                        <li>Contato e definição de requisitos e prefêrencias do produto com o cliente.</li>
+                        <li>Metodologia ágil SCRUM. </li>
+                        <li>Desenvolvimento WEB fullstack.</li>
+                        <li>Análise de dados.   </li>
+                    </ul>
+                </section>
+                <section className={styles.formation2}>
+                    <h2><b>Science and Business - (PIT) </b></h2>
+                    <h3>Apresentação de projeto para reconhecimento de resíduos descartaveis através de
+                        câmera (Plástico, Papelão, Metal e Baterias). | Maio 2024 |</h3> <br />
+                    <ul>
+                        <li> Python / YOLOv8 / OpenCV / Google COLAB </li>
+                        <li>Contribuição para desenvolvimento da lógica de reconhecimento, gerenciamento
+                            do banco de imagens e rotulação. </li>
+                    </ul>
+                </section>
+
+                <section className={styles.formation1}>
+                    <h2><b>Infinity Academy 3D</b></h2>
+                    <h3>Desenvolvedor Unity | Junho 2024 – Outubro 2024 | </h3>
+                    <ul>
+                        <li>Implementação de projetos com interação de VR e AR</li>
+                        <li>Criação de cenários 3D </li>
+                        <li>Gerenciamento de bibliotecas e soluções </li>
+                    </ul>
+                </section>
+                <section className={styles.formation2}>
+                    <h2><b>Micro Assist Informática</b></h2>
+                    <h3>Suporte nível 1 | Dezembro 2023 - Fevereiro 2024 |</h3> <br />
+                    <ul>
+                        <li> Suporte Nivel 1  </li>
+                        <li>Costumer Experience - Expêriencia em tratamento ao cliente.</li>
+                        <li>Gestões de TI – Manutenção de computadores, notebooks e impressoras,
+                            mapeamento de rede, resolução de problemas, pacote Office.  </li>
+                        <li> Atendimento de clientes via chat. </li>
+                        <li>Certificado de conclusão do onbording na empresa. </li>
                     </ul>
                 </section>
             </section>
